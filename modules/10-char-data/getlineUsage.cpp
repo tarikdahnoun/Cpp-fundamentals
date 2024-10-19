55965a6d8225fc547907a79382bf397bf7d88e30
Tarik Dahnoun <tarik.dahnoun@gmail.com>
2024-10-19 16:23:03 -0700

    getline for strings vs cstrings

--- modules/10-char-data/getlineUsage.cpp
+++ modules/10-char-data/getlineUsage.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main ()
 {
+    // This is how its done with C++:
     string name1, name2;
     
     // Get a name.
@@ -19,10 +20,24 @@ int main ()
     // Display them in alphabetical order.
     cout << "Here are the names sorted alphabetically:\n";
     if (name1 < name2)
-    cout << name1 << endl << name2 << endl;
+    {
+        cout << name1 << endl << name2 << endl;
+    }
     else if (name1 > name2)
-    cout << name2 << endl << name1 << endl;
+    {
+        cout << name2 << endl << name1 << endl;
+    }
     else
-    cout << "You entered the same name twice!\n";
+    {
+        cout << "You entered the same name twice!\n";
+    }
+
+    // This is how its done with C-strings:
+    const int MAX_CHARS = 128; 
+    char name[MAX_CHARS];
+    cout << "First and last name separated by a space: \n";
+    cin.getline(name, MAX_CHARS);
+    cout << "Hello, " << name << "!\n";
+
     return 0;
 }
