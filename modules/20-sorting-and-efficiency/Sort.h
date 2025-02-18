ed212e08e75ae630555d8acd2b00133879b9d294
Tarik Dahnoun <tarik.dahnoun@gmail.com>
2025-02-18 07:23:37 -0800

    insertion sort

--- modules/20-sorting-and-efficiency/Sort.h
+++ modules/20-sorting-and-efficiency/Sort.h
@@ -25,6 +25,12 @@ class Sort
 
         template <typename T>
         void selectionSortDec(vector<T>& arr);
+
+        template <typename T>
+        void insertionSortInc(vector<T>& arr);
+
+        template <typename T>
+        void insertionSortDec(vector<T>& arr);
 };
 
 template <typename T>
@@ -55,7 +61,7 @@ void Sort::bubbleSortInc(vector<T>& arr)
         {
             if (arr[i] > arr[i + 1])
             {
-                cout << "INFO: Indices " << i << ", " << i + 1 
+                cout << "DEBUG: Indices " << i << ", " << i + 1 
                      << " values are " << arr[i] << " > " << arr[i + 1]
                      << ". Swapping the two values. New array: ";
                 swap(arr[i], arr[i + 1]);
@@ -86,7 +92,7 @@ void Sort::bubbleSortDec(vector<T>& arr)
         {
             if (arr[i] < arr[i + 1])
             {
-                cout << "INFO: Indices " << i << ", " << i + 1 
+                cout << "DEBUG: Indices " << i << ", " << i + 1 
                      << " values are " << arr[i] << " < " << arr[i + 1]
                      << ". Swapping the two values. New array: ";
                 swap(arr[i], arr[i + 1]);
@@ -117,13 +123,13 @@ void Sort::selectionSortInc(vector<T>& arr)
             if (arr[j] < arr[minIndex])
             {
                 minIndex = j;
-                cout << "INFO: Found new min: " << arr[j] << " in position: " << j << ". \n";
+                cout << "DEBUG: Found new min: " << arr[j] << " in position: " << j << ". \n";
             }
         }
-        cout << "INFO: Swapping current (index, value): (" << i << ", " << arr[i]
+        cout << "DEBUG: Swapping current (index, value): (" << i << ", " << arr[i]
              << ") with minimum (index, value): (" << minIndex << ", " << arr[minIndex] << ").\n";
         swap(arr[minIndex], arr[i]);
-        cout << "INFO: New Array: ";
+        cout << "DEBUG: New Array: ";
         printArr(arr);
     }
     cout << endl << "INFO: Sorted array: ";
@@ -148,15 +154,83 @@ void Sort::selectionSortDec(vector<T>& arr)
             if (arr[j] > arr[maxIndex])
             {
                 maxIndex = j;
-                cout << "INFO: Found new max: " << arr[j] << " in position: " << j << ". \n";
+                cout << "DEBUG: Found new max: " << arr[j] << " in position: " << j << ". \n";
             }
         }
-        cout << "INFO: Swapping current (index, value): (" << i << ", " << arr[i]
+        cout << "DEBUG: Swapping current (index, value): (" << i << ", " << arr[i]
              << ") with maximum (index, value): (" << maxIndex << ", " << arr[maxIndex] << ").\n";
         swap(arr[maxIndex], arr[i]);
-        cout << "INFO: New Array: ";
+        cout << "DEBUG: New Array: ";
         printArr(arr);
     }
+    cout << endl << "INFO: Sorted array: ";
+    printArr(arr);
+}
+
+template <typename T>
+void Sort::insertionSortInc(vector<T>& arr)
+{
+    cout << "-----------------------\n"
+         << "   Insertion Sort Inc   \n"
+         << "-----------------------\n";
+    cout << "INFO: Unsorted array: ";
+    printArr(arr);
+    cout << endl;
+
+    for (size_t i = 1; i < arr.size(); i++)
+    {
+        T nextItem = arr[i];
+        int loc = i;
+        while ((loc > 0) && (arr[loc - 1] > nextItem))
+        {
+            arr[loc] = arr[loc - 1];
+            loc--;
+        }
+        arr[loc] = nextItem;
+        cout << "DEBUG: Moving element (index, value): (" 
+             << i << ", " << arr[i]
+             << ") -> (" 
+             << loc << ", "
+             << arr[loc] << ")"
+             << endl
+             << "DEBUG: New Array: ";
+             printArr(arr);
+    }
+    
+    cout << endl << "INFO: Sorted array: ";
+    printArr(arr);
+}
+
+template <typename T>
+void Sort::insertionSortDec(vector<T>& arr)
+{
+    cout << "-----------------------\n"
+         << "   Insertion Sort Dec   \n"
+         << "-----------------------\n";
+    cout << "INFO: Unsorted array: ";
+    printArr(arr);
+    cout << endl;
+
+    for (size_t i = 1; i < arr.size(); i++)
+    {
+        T nextItem = arr[i];
+        int loc = i;
+        while ((loc > 0) && (arr[loc - 1] < nextItem))
+        {
+            arr[loc] = arr[loc - 1];
+            loc--;
+        }
+        arr[loc] = nextItem;
+        cout << "DEBUG: Moving element (index, value): (" 
+             << i << ", " << arr[i]
+             << ") -> (" 
+             << loc << ", "
+             << arr[loc] << ")"
+             << endl
+             << "DEBUG: New Array: ";
+             printArr(arr);
+    }
+    
     cout << endl << "INFO: Sorted array: ";
     printArr(arr);
 }
