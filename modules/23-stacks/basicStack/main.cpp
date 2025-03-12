288113ec9b3159cdafc5a79cab37181202250d81
Tarik Dahnoun <tarik.dahnoun@gmail.com>
2025-03-11 22:35:15 -0700

    linked list stack

--- modules/23-stacks/basicStack/main.cpp
+++ modules/23-stacks/basicStack/main.cpp
@@ -1,19 +1,26 @@
 #include "ArrayStack.h"
+#include "LinkedListStack.h"
 #include <iostream>
 
 int main() {
-    ArrayStack<int> stack;
+    ArrayStack<int> arrayStack;
+    LinkedListStack<int> linkedListStack;
 
-    std::cout << "Pushing 10, 20, 30...\n";
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
+    std::cout << "Testing ArrayStack...\n";
+    arrayStack.push(10);
+    arrayStack.push(20);
+    arrayStack.push(30);
+    std::cout << "Top of ArrayStack: " << arrayStack.peek() << std::endl;
+    arrayStack.pop();
+    std::cout << "New top of ArrayStack: " << arrayStack.peek() << std::endl;
 
-    std::cout << "Top of stack: " << stack.peek() << std::endl;
-
-    std::cout << "Popping top element...\n";
-    stack.pop();
-    std::cout << "New top: " << stack.peek() << std::endl;
+    std::cout << "\nTesting LinkedListStack...\n";
+    linkedListStack.push(10);
+    linkedListStack.push(20);
+    linkedListStack.push(30);
+    std::cout << "Top of LinkedListStack: " << linkedListStack.peek() << std::endl;
+    linkedListStack.pop();
+    std::cout << "New top of LinkedListStack: " << linkedListStack.peek() << std::endl;
 
     return 0;
 }
