e9e3dd5e41a9fa30a0c2e2a1a02f2f4266fee76e
tarikdahnoun <tarik.dahnoun@gmail.com>
2025-05-12 06:59:21 -0700

    graph implementation and airport example

--- modules/29-graphs/cs-airlines/main.cpp
+++ modules/29-graphs/cs-airlines/main.cpp
@@ -1,12 +1,20 @@
-// Tarik Dahnoun
-// Module 13 Assignment: CS Airlines
-// Use Dijkstra's algorithm to determine the cheapest flight from San Franciso to other cities. 
+#include "Graph/Graph.cpp"
+#include <vector>
+#include <string>
 
+int main() {
+    std::vector<std::string> cityNames = {"SFO", "LAX", "LAS", "PHX", "DEN", "JFK"};
+    Graph<std::string> graph(6, cityNames);
 
+    graph.add("SFO", "LAX", 120);
+    graph.add("SFO", "LAS", 150);
+    graph.add("SFO", "JFK", 500);
+    graph.add("LAX", "PHX", 75);
+    graph.add("LAX", "JFK", 350);
+    graph.add("LAS", "DEN", 250);
+    graph.add("PHX", "DEN", 150);
 
-int main()
-{
-	
-	return 0;
+    graph.dijkstra("SFO");
+
+    return 0;
 }
-
